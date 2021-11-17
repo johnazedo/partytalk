d651579ecfa787dfc1388f0316463219ffc6cd90
João Pedro Limão <jplimao12@gmail.com>
2021-11-17 20:56:47 -0300

    Use provider to dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 import 'package:talk/ui/auth/auth_screen.dart';
+import 'package:talk/ui/auth/auth_viewmodel.dart';
 import 'package:talk/ui/constants/strings.dart';
 
 void main() {
@@ -22,14 +24,19 @@ class MyApp extends StatelessWidget {
       ),
     );
 
-    return MaterialApp(
-      title: Strings.appName,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: Strings.fontFamily,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthViewModel())
+      ],
+      child: MaterialApp(
+        title: Strings.appName,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          fontFamily: Strings.fontFamily,
+        ),
+        home: const AuthScreen(),
       ),
-      home: const AuthScreen(),
     );
   }
 }
--- lib/ui/auth/auth_screen.dart
+++ lib/ui/auth/auth_screen.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/src/provider.dart';
 import 'package:talk/ui/auth/auth_viewmodel.dart';
 import 'package:talk/ui/constants/dimens.dart';
 import 'package:talk/ui/constants/strings.dart';
 import 'package:talk/ui/constants/styles.dart';
 
-class AuthScreen extends StatefulWidget {
+class AuthScreen extends StatelessWidget {
   const AuthScreen({Key? key}) : super(key: key);
 
   @override
-  State<AuthScreen> createState() => _AuthScreenState();
-}
-
-class _AuthScreenState extends State<AuthScreen> {
-  var vm = AuthViewModel();
-
-  @override
   Widget build(BuildContext context) {
+    final vm = context.watch<AuthViewModel>();
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(Dimens.screenPadding),
@@ -30,20 +25,20 @@ class _AuthScreenState extends State<AuthScreen> {
               Strings.appDescription,
               style: TextStyles.descriptionApp,
             ),
-
             Center(
               child: ElevatedButton(
                 child: const Text(Strings.googleTextButton),
                 onPressed: vm.signIn,
               ),
             ),
-
             Center(
-                child: AnimatedBuilder(
-                    animation: vm,
-                    builder: (context, child) {
-                      return Text("Log: User = ${vm.nameUser.value}");
-                    })),
+              child: AnimatedBuilder(
+                animation: vm,
+                builder: (context, child) {
+                  return Text("Log: User = ${vm.nameUser.value}");
+                },
+              ),
+            ),
           ],
         ),
       ),
