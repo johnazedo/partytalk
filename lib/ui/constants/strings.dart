981c68cdba8d483ca77d3c7d48f1b7d9a0b7c4f5
João Pedro Limão <jplimao12@gmail.com>
2021-11-18 15:55:44 -0300

    Change app font

--- lib/ui/constants/strings.dart
+++ lib/ui/constants/strings.dart
@@ -3,9 +3,12 @@
 class Strings {
   // Shared
   static const appName = "PartyTalk";
-  static const fontFamily = "Inter";
+  static const fontFamily = "Avenir";
 
   // AuthScreen
   static const googleTextButton = "Entrar com o Google";
   static const appDescription = "O aplicativo de conversas para o seu evento. Efetue o login para expandir o seu network.";
+
+  // ChatsScreen
+  static const chats = "Conversas";
 }
