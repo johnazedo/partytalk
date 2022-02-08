95f2be0ec4dafa83730ec1e195038bd8746ab043
rodolfo <rodolfo.oliveira.106@ufrn.edu.br>
2022-02-08 00:18:31 -0300

    new: search contact

--- lib/ui/screens/participants/components/contacts_letter_item.dart
+++ lib/ui/screens/participants/components/contacts_letter_item.dart
@@ -7,9 +7,13 @@ import 'package:talk/ui/constants/styles.dart';
 class ContactsLetterItem extends StatefulWidget {
   final String letter;
   final List<User> users;
+  final String search;
 
   const ContactsLetterItem(
-      {Key? key, required this.letter, required this.users})
+      {Key? key,
+      required this.letter,
+      required this.users,
+      required this.search})
       : super(key: key);
 
   @override
@@ -20,7 +24,8 @@ class _ContactsLetterItemState extends State<ContactsLetterItem> {
   @override
   Widget build(BuildContext context) {
     return Visibility(
-      visible: widget.users.isNotEmpty,
+      visible: widget.users.isNotEmpty &&
+          letterIsOnSearch(widget.search, widget.letter),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -39,34 +44,38 @@ class _ContactsLetterItemState extends State<ContactsLetterItem> {
             itemBuilder: (context, index) {
               return Padding(
                 padding: const EdgeInsets.only(bottom: 3),
-                child: ListTile(
-                  onTap: () => Navigator.of(context).pushNamed(
-                    "/messages",
-                    arguments: ToMessageArguments(
-                      displayName: widget.users[index].name,
-                      email: widget.users[index].email,
-                      photoURL: widget.users[index].photoURL,
-                    ),
-                  ),
-                  leading: Container(
-                    width: 50,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                        image: getImage(widget.users[index].photoURL),
-                        fit: BoxFit.cover,
+                child: Visibility(
+                  visible:
+                      nameIsOnSearch(widget.search, widget.users[index].name),
+                  child: ListTile(
+                    onTap: () => Navigator.of(context).pushNamed(
+                      "/messages",
+                      arguments: ToMessageArguments(
+                        displayName: widget.users[index].name,
+                        email: widget.users[index].email,
+                        photoURL: widget.users[index].photoURL,
                       ),
                     ),
+                    leading: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: getImage(widget.users[index].photoURL),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      widget.users[index].name,
+                      style: TextStyles.headLine1,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    subtitle: Text('@' + widget.users[index].username,
+                        style: TextStyles.body1),
                   ),
-                  title: Text(
-                    widget.users[index].name,
-                    style: TextStyles.headLine1,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  subtitle: Text('@' + widget.users[index].username,
-                      style: TextStyles.body1),
                 ),
               );
             },
@@ -82,4 +91,22 @@ ImageProvider getImage(String? photoURL) {
     return const AssetImage("assets/images/avatar.jpg");
   }
   return NetworkImage(photoURL);
+}
+
+bool letterIsOnSearch(String s, String letter) {
+  return s.toUpperCase().contains(letter) || s.isEmpty;
+}
+
+bool nameIsOnSearch(String s, String name) {
+  if (s.isEmpty) {
+    return true;
+  }
+  var contain = true;
+  var nameUpper = name.toUpperCase();
+  for (var char in s.characters.toUpperCase()) {
+    if (!nameUpper.contains(char)) {
+      contain = false;
+    }
+  }
+  return contain;
 }
--- lib/ui/screens/participants/participants_viewmodel.dart
+++ lib/ui/screens/participants/participants_viewmodel.dart
@@ -3,14 +3,20 @@ import 'package:talk/domain/entities/user.dart';
 import 'package:talk/domain/usecases/get_participants.dart';
 
 class EventParticipantsViewModel extends ChangeNotifier {
-
   var participants = ValueNotifier<Map<String, List<User>>>({});
+  var searchField = ValueNotifier<String>("");
 
   final EventParticipantsUseCase eventsParticipantsUseCase;
   EventParticipantsViewModel({required this.eventsParticipantsUseCase});
 
   getParticipants(String eventId) async {
-    participants.value = await eventsParticipantsUseCase.getParticipants(eventId);
+    participants.value =
+        await eventsParticipantsUseCase.getParticipants(eventId);
+    notifyListeners();
+  }
+
+  setSearchField(String search) {
+    searchField.value = search;
     notifyListeners();
   }
 }
